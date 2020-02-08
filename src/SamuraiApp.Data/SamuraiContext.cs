df2b714f0457efe50e1e21e1dddd981a1c232c20
SimonBane <simeon.banev3@gmail.com>
2020-02-08 12:49:47 +0200

    Initial project commit.

---
+++ src/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain.Entities;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog = SamuraiApp;Integrated Security=SSPI;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
+    }
+}
---
+++ src/SamuraiApp.Domain/Entities/Samurai.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SamuraiApp.Domain.Entities
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; } = new List<Quote>();
+        public Clan Clan { get; set; }
+        public List<SamuraiBattle> SamuraiBattles { get; set; }
+        public Horse Horse { get; set; }
+    }
+}
